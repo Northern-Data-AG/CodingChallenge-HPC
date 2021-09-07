8b31ee0ec608be83f094b49b036c95f511a356fe
Chanuaran Santhirasegaram <chanuaran@googlemail.com>
2021-09-07 11:53:34 +0200

    init commit

---
+++ run.py
@@ -0,0 +1,9 @@
+# Load data
+import mnist_loader
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+training_data = list(training_data)
+
+# Build network
+import network as network
+net = network.Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
